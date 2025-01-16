91294b0bf4f778f3fc4141ab9fd3fc91e738b641
GHR <g.rosuson@gmail.com>
2025-01-16 15:23:09 +0100

    feat(frontend): extend routing

--- frontend/src/components/routing/routes/Routes.tsx
+++ frontend/src/components/routing/routes/Routes.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes as ReactRouterDOMRoutes } from 'react-router-dom';
 
-import Dashboard from 'components/pages/dashboard/Dashboard';
+import Authentication from 'components/pages/authentication/Authentication';
 import Home from 'components/pages/home/Home';
 
 const Routes = () => {
     return (
         <ReactRouterDOMRoutes>
+            {/* Routes that don't require authentication */}
+            <Route path="/login" element={<Authentication/>} />
+            <Route path="/register" element={<Authentication/>} />
+
+            {/* Routes that require authentication */}
             <Route path="/" element={<Home/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
         </ReactRouterDOMRoutes>
     )
 };

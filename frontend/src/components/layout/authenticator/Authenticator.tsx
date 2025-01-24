401b283fcb0c2dbe159ae96ad6e550f65bed2f60
GHR <g.rosuson@gmail.com>
2025-01-24 13:19:43 +0100

    feat(frontend): update authenticator comp

--- frontend/src/components/layout/authenticator/Authenticator.tsx
+++ frontend/src/components/layout/authenticator/Authenticator.tsx
@@ -1,25 +1,51 @@
-import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { useCallback,useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import TopBar from '../topBar/TopBar';
 
 import api from 'api';
+import config from 'config';
+import { actions,useStore } from 'store';
+import utils from 'utils';
 
-const Authenticator = ({ children }: { children: ReactNode}) => {
-    const [state, setState] = useState({
-        accessToken: null,
+const Authenticator = () => {
+    // State
+    const [{ isLoading }, setState] = useState({
         isLoading: true
     });
 
 
-    const fetchTokens = useCallback(async () => {
+    // Store
+    const store = useStore();
+
+
+    // Hooks
+    const navigate = useNavigate();
+
+
+    /**
+     * - Validates the "accessToken" in the global state.
+     * - Renews the "accessToken" when invalid or missing and re-routes
+     *   the user to the login page when the renewal is rejected.
+     */
+    const authenticate = useCallback(async () => {
         try {
-            const response = await api.service.resources.authentication.refreshAccessToken();
+            const isJwtValid = !!store.authentication.accessToken && utils.jwt.isValid(store.authentication.accessToken);
 
-            setState(prevState => ({
-                ...prevState,
-                accessToken: response.accessToken || null
-            }));
+            if (!isJwtValid) {
+                // Note: "refreshAccessToken" will throw an Error if unsuccessful
+                const response = await api.service.resources.authentication.refreshAccessToken();
 
+                const payload = {
+                    payload: response.accessToken,
+                    type: actions.authentication.change_access_token
+                }
+
+                store.dispatch(payload)
+            }
         } catch (error) {
-            console.error(error)
+            console.error(error);
+            navigate(config.routes.login);
 
         } finally {
             setState(prevState => ({
@@ -27,15 +53,33 @@ const Authenticator = ({ children }: { children: ReactNode}) => {
                 isLoading: false
             }));
         }
-    }, [state.accessToken]);
+    },[]);
 
 
+    /**
+     * Invokes "authenticate" on-mount.
+     */
     useEffect(() => {
-        fetchTokens();
-    }, [fetchTokens])
+        authenticate();
+    }, [authenticate]);
 
 
-    return children;
+    // Determine views
+    const spinner = (
+        <div>
+            Loading...
+        </div>
+    )
+
+    const component = (
+        <>
+            <TopBar/>
+            <Outlet/>
+        </>
+    );
+
+
+    return isLoading ? spinner : component;
 };
 
 export default Authenticator;

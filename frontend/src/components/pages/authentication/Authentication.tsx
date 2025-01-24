2e956466b81de88b7cac09be7d43e33cb65e36af
GHR <g.rosuson@gmail.com>
2025-01-24 13:14:42 +0100

    feat(frontend): add authentication page

---
+++ frontend/src/components/pages/authentication/Authentication.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect,useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+import Button from 'components/UI/button/Button';
+import Input from 'components/UI/input/Input';
+
+import api from 'api';
+import config from 'config';
+import { actions,useStore } from 'store';
+import utils from 'utils';
+
+import styling from './Authentication.module.scss';
+
+import constants from './constants';
+
+const Authentication = () => {
+    // State
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+    });
+
+
+    // Store
+    const store = useStore();
+
+
+    // Hooks
+    const location = useLocation();
+    const navigate = useNavigate();
+
+
+    // Flags
+    const isLoginActive = location.pathname === config.routes.login;
+
+
+
+    /**
+     * Sets the input field changes in the state.
+     */
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+
+    /**
+     * Sets the "accessToken" in the global state on login and register.
+     */
+    const onSubmit = async (event: React.FormEvent) => {
+        try {
+            event.preventDefault();
+
+            const submitFn = isLoginActive ? api.service.resources.authentication.login : api.service.resources.authentication.register;
+
+            const response = await submitFn(state);
+
+            const payload = {
+                payload: response.accessToken,
+                type: actions.authentication.change_access_token
+            }
+
+            store.dispatch(payload)
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+   /**
+    * Navigate to root when an "accessToken" is set.
+    */
+   useEffect(() => {
+        if (store.authentication.accessToken && utils.jwt.isValid(store.authentication.accessToken)) {
+            navigate(config.routes.root);
+        }
+    }, [store.authentication.accessToken]);
+
+
+
+    // Headings, labels and route
+    const heading = isLoginActive ? constants.labels.heading.login : constants.labels.heading.register;
+    const buttonLabel =  isLoginActive ? constants.labels.button.login : constants.labels.button.register;
+    const authModeLinkLabel =  isLoginActive ? constants.labels.links.register : constants.labels.links.login;
+    const route = isLoginActive ? config.routes.register : config.routes.login;
+
+
+    return (
+        <div className={styling.container}>
+            <h2>{heading}</h2>
+
+            <form className={styling.form} onSubmit={onSubmit}>
+                <Input
+                    label={constants.labels.input.email.label}
+                    type="email"
+                    name="email"
+                    value={state.email}
+                    placeholder={constants.labels.input.email.placeholder}
+                    onChange={onInputChange}
+                />
+
+                <Input
+                    label={constants.labels.input.password.label}
+                    type="password"
+                    name="password"
+                    value={state.password}
+                    placeholder={constants.labels.input.password.placeholder}
+                    onChange={onInputChange}
+                />
+
+                <Button type="submit" addTopMargin>
+                    {buttonLabel}
+                </Button>
+            </form>
+
+            <div className={styling.link}>
+                <Link to={route}>
+                    {authModeLinkLabel}
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Authentication;
---
+++ frontend/src/components/pages/authentication/constants/index.ts
@@ -0,0 +1,28 @@
+const constants = {
+    labels: {
+        heading: {
+            login: 'Login',
+            register: 'Register'
+        },
+        button: {
+            login: 'Login',
+            register: 'Register'
+        },
+        input: {
+            email: {
+                label: 'Email',
+                placeholder: 'E.g. example@mail.com'
+            },
+            password: {
+                label: 'Password',
+                placeholder: 'Enter password...'
+            }
+        },
+        links: {
+            login: 'Already have an account? Log in here.',
+            register: 'Dont have an account? Register here.'
+        }
+    }
+}
+
+export default constants;

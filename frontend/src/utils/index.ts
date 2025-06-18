f4c03c77646fe5c0807b9f051374635db74b937d
GHR <g.rosuson@gmail.com>
2025-06-18 11:45:13 +0200

    feat(frontend): add throttle util fn

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,9 +1,11 @@
 import jwt from './jwt';
 import time from './time';
+import validators from './validators';
 
 const utils = {
     time,
-    jwt
+    jwt,
+    validators
 };
 
 export default utils;
--- frontend/src/utils/time/index.ts
+++ frontend/src/utils/time/index.ts
@@ -1,12 +1,36 @@
 /**
  * Returns a promise that resolves after the given time,
+ * useful for introductin articial delay before code execution.
  */
 const sleep = (time = 250) => new Promise((resolve) => {
     setTimeout(resolve, time);
 });
 
+/**
+ * Throttles a function so it's only allowed to run once every X ms,
+ * useful for blocking rapid consecutive calls (e.g., theme toggle).
+ */
+const throttle = <T extends (...args: any[]) => void>(fn: T, delay = 500) => {
+    let ready = true;
+
+    return (...args: Parameters<T>) => {
+        if (!ready) {
+            return;
+        }
+
+        ready = false;
+        
+        fn(...args);
+
+        setTimeout(() => {
+            ready = true;
+        }, delay);
+    };
+};
+
 const time = {
-    sleep
+    sleep,
+    throttle
 };
 
 export default time;

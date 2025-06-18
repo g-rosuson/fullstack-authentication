b99c4c56b209756f42ebb1afa50ae2c052485d9c
GHR <g.rosuson@gmail.com>
2025-06-18 11:44:38 +0200

    feat(frontend): add warning logging fn

--- frontend/src/services/logging/index.ts
+++ frontend/src/services/logging/index.ts
@@ -9,8 +9,20 @@ const error = (error: Error) => {
     }
 };
 
+/**
+ * Logs a warning to the console when developing.
+ */
+const warning = (message: string) => {
+    const isDeveloping = window.location.hostname === 'localhost';
+
+    if (isDeveloping) {
+        console.warn(message);
+    }
+};
+
 const logging = {
-    error
+    error,
+    warning
 };
 
 export default logging;

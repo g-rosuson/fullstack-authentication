240af7a8bd707d59afb87ec149ecade50659a2c9
GHR <g.rosuson@gmail.com>
2025-06-27 01:39:01 +0200

    chore(frontend): integrate new types into api methods

--- frontend/src/api/service/resources/authentication/index.ts
+++ frontend/src/api/service/resources/authentication/index.ts
@@ -1,4 +1,4 @@
-import { AuthenticationOutputDto, LoginInputDto, RegisterInputDto } from '_types/_gen';
+import { AccessToken, LoginUserPayload, RegisterUserPayload } from '_types/_gen';
 import { ApiResponse } from '_types/infrastructure';
 
 import client from '../../client';
@@ -9,23 +9,23 @@ import config from './config';
  * refresh token cookie is sent to the backend end-point.
  */
 const refreshAccessToken = async () => {
-    return await client.get<ApiResponse<AuthenticationOutputDto>>(config.path.refresh);
+    return await client.get<ApiResponse<AccessToken>>(config.path.refresh);
 };
 
 /**
  * Creates a new user and retrieves an access-token when valid credentials
  * are sent to the register backend endpoint.
  */
-const register = async (credentials: RegisterInputDto) => {
-    return await client.post<ApiResponse<AuthenticationOutputDto>, RegisterInputDto>(config.path.register, credentials);
+const register = async (credentials: RegisterUserPayload) => {
+    return await client.post<ApiResponse<AccessToken>, RegisterUserPayload>(config.path.register, credentials);
 };
 
 /**
  * Retrieves a new access-token when valid credentials are sent
  * to the login backend endpoint.
  */
-const login = async (credentials: LoginInputDto) => {
-    return await client.post<ApiResponse<AuthenticationOutputDto>, LoginInputDto>(config.path.login, credentials);
+const login = async (credentials: LoginUserPayload) => {
+    return await client.post<ApiResponse<AccessToken>, LoginUserPayload>(config.path.login, credentials);
 };
 
 /**

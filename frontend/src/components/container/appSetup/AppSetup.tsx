2e227789ddbcc2c65e74cafd4e03427d9894facd
GHR <g.rosuson@gmail.com>
2025-06-27 03:17:19 +0200

    refactor(frontend): move ui store intialization into app-setup comp

--- frontend/src/components/container/appSetup/AppSetup.tsx
+++ frontend/src/components/container/appSetup/AppSetup.tsx
@@ -1,14 +1,44 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { useUserInterfaceSelection } from 'store/selectors/ui';
 
 import config from 'config';
+import storage from 'services/storage';
 
 const AppSetup = ({ children }: { children: ReactNode }) => {
-    // Set backend URL on the global object
+    // UI store
+    const { theme, changeTheme } = useUserInterfaceSelection();
+
+    // * Initialize theme
+    // Get persisted theme from local storage
+    const persistedTheme = storage.getTheme();
+
+    // Check if the system theme is dark
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // If no theme persisted, use system preference
+    const themeToApply = persistedTheme || (prefersDark ? 'dark' : 'light');
+
+    // Set data-theme attribute
+    document.documentElement.setAttribute('data-theme', themeToApply);
+
+    // * Set backend URL on the global object
     const isDeveloping = window.location.hostname === 'localhost';
     const backendRootUrl = isDeveloping ? config.connect.backend.dev.url : config.connect.backend.prod.url;
 
     window.metadata = window.metadata ?? {};
     window.metadata.backendRootUrl = backendRootUrl;
+
+
+    /**
+     * Updates the store theme only if it 
+     * from the resolved initial theme.
+     */
+    useEffect(() => {
+        if (themeToApply !== theme) {
+            changeTheme(themeToApply);
+        }
+    }, [changeTheme, themeToApply, theme]);
+
 
     return children;
 };
--- frontend/src/store/slices/ui/index.ts
+++ frontend/src/store/slices/ui/index.ts
@@ -1,33 +1,14 @@
 import { Theme } from 'shared/types/theme';
 import { StateCreator } from 'zustand';
 
-import storage from 'services/storage';
-
 import { Store } from '../../store.types';
 import { UserInterfaceSlice } from './ui.types';
 
-export const createUserInterfaceSlice: StateCreator<Store, [], [], UserInterfaceSlice> = (set) => {
-    // * Initialize the store theme
-
-    // Get persisted theme from local storage
-    const persistedTheme = storage.getTheme();
-
-    // Check if the system theme is dark
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    // If no theme persisted, use system preference
-    const themeToApply = persistedTheme || (prefersDark ? 'dark' : 'light');
-
-    // Set data-theme attribute
-    document.documentElement.setAttribute('data-theme', themeToApply);
-
-    
+export const createUserInterfaceSlice: StateCreator<Store, [], [], UserInterfaceSlice> = (set) => {  
     return {
-        theme: themeToApply,
+        theme: 'dark',
         isSidebarOpen: true,
-        toggleSidebar: () => set(store => ({ 
-            isSidebarOpen: !store.isSidebarOpen 
-        })),
+        toggleSidebar: () => set(store => ({ isSidebarOpen: !store.isSidebarOpen })),
         changeTheme: (theme: Theme) => set(() => ({ theme }))
     }
 };

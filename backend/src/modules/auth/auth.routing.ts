6419a68fb881edb5531c7ee1a1b27eea90d7b7d7
GHR <g.rosuson@gmail.com>
2025-07-06 18:55:24 +0200

    chore(backend): integrate middleware

--- backend/src/modules/auth/auth.routing.ts
+++ backend/src/modules/auth/auth.routing.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { login, logout, register, renewAccessToken } from './auth.controller';
 import { validateAuthenticationInput, validateRefreshToken } from './auth.middleware';
 import { loginLimiter, refreshLimiter, registerLimiter } from 'shared/middleware/rate-limiter';
+import validateUserInput from 'shared/middleware/validate-user-input';
 
 import config from './auth.config';
 
@@ -11,7 +12,7 @@ const router = Router();
 
 // Determine routes
 router.post(config.route.login, loginLimiter, validateAuthenticationInput, login);
-router.post(config.route.register, registerLimiter, validateAuthenticationInput, register);
+router.post(config.route.register, registerLimiter, validateUserInput, validateAuthenticationInput, register);
 router.post(config.route.logout, validateRefreshToken, logout);
 router.get(config.route.refresh, refreshLimiter, validateRefreshToken, renewAccessToken);
 

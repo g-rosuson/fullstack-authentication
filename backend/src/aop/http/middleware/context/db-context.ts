7bdb46f52cd6de406889e41d7fd229f0c5689743
GHR <g.rosuson@gmail.com>
2025-11-04 10:55:20 +0100

    chore(backend): small fixes

--- backend/src/aop/http/middleware/context/db-context.ts
+++ backend/src/aop/http/middleware/context/db-context.ts
@@ -3,10 +3,20 @@ import { DbContext } from 'aop/db/mongo/context';
 
 import type { NextFunction, Request, Response } from 'express';
 
+/**
+ * Injects the database context into the request context.
+ * @param req Express request object
+ * @param _res Express response object
+ * @param next Express next function
+ */
 const dbContextMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
     const manager = MongoClientManager.getInstance();
     const db = await manager.connect();
-    req.context.db = new DbContext(db);
+
+    req.context = {
+        ...req.context,
+        db: new DbContext(db),
+    };
 
     next();
 };

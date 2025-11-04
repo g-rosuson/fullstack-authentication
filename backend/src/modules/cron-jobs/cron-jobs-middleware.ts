beb8debad66a519765db6061fdf220c5593965c1
GHR <g.rosuson@gmail.com>
2025-11-04 12:01:56 +0100

    feat(backend): add/integrate time parsing utility

--- backend/src/modules/cron-jobs/cron-jobs-middleware.ts
+++ backend/src/modules/cron-jobs/cron-jobs-middleware.ts
@@ -6,6 +6,7 @@ import { ErrorMessage } from 'shared/enums/error-messages';
 
 import { cronJobPayloadSchema } from './schemas';
 import { idRouteParamSchema, paginatedRouteParamSchema } from 'shared/schemas/routeParam';
+import { parseDate } from 'utils/time';
 
 /**
  * Validates that the request body adhears to the corresponding schema.
@@ -14,9 +15,18 @@ import { idRouteParamSchema, paginatedRouteParamSchema } from 'shared/schemas/ro
  * @param next Express next function
  */
 const validatePayload = (req: Request, _res: Response, next: NextFunction) => {
+    // Parse date strings into Date objects
+    const parsedPayload = {
+        ...req.body,
+        time: parseDate(req.body.time),
+        startDate: parseDate(req.body.startDate),
+        endDate: req.body.endDate ? parseDate(req.body.endDate) : null,
+    };
+
+    // Validate the payload against the schema
     const validatedPayload = validateRequestPayload(
         cronJobPayloadSchema,
-        req.body,
+        parsedPayload,
         ErrorMessage.CRON_JOB_SCHEMA_VALIDATION_FAILED
     );
 

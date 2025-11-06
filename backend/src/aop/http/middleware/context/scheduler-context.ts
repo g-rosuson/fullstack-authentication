1983d4ee471fa39c1b0b74c287fa3e723dd44f48
GHR <g.rosuson@gmail.com>
2025-11-06 16:51:43 +0100

    chore(backend): remove duplicate

--- backend/src/aop/http/middleware/context/scheduler-context.ts
+++ backend/src/aop/http/middleware/context/scheduler-context.ts
@@ -3,6 +3,12 @@ import { SchedulerContext } from 'aop/scheduler/context';
 
 import type { NextFunction, Request, Response } from 'express';
 
+/**
+ * Injects the scheduler instance into the request context.
+ * @param req Express request object
+ * @param _res Express response object
+ * @param next Express next function
+ */
 const schedulerContextMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
     const schedulerInstance = Scheduler.getInstance();
     req.context.scheduler = new SchedulerContext(schedulerInstance);

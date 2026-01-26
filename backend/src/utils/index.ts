0636dc60042cdc9d046a979e4544d0df17c0ad72
GHR <g.rosuson@gmail.com>
2026-01-26 11:52:22 +0100

    chore(backend): adjust util fn name

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,5 +1,5 @@
 import { retryWithFixedInterval } from './async/utils-async-retry';
-import { kebabToCamel } from './string';
+import { kebabToCamelCase } from './string';
 import { isObject } from './validation/utils-validation';
 
-export { retryWithFixedInterval, isObject, kebabToCamel };
+export { retryWithFixedInterval, isObject, kebabToCamelCase };

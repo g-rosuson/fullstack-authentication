fa6b89586ef4c58b7bbfc8429644a7ace715b30c
GHR <g.rosuson@gmail.com>
2026-02-21 21:24:41 +0100

    feat(frontend): add jobs module

---
+++ frontend/src/components/pages/jobs/Jobs.tsx
@@ -0,0 +1,11 @@
+import Heading from 'components/UI/heading/Heading';
+
+const Jobs = () => {
+    return (
+        <div>
+            <Heading size='l' level={2}>Jobs</Heading>
+        </div>
+    );
+};
+
+export default Jobs;

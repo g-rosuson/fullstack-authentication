4c2c9fd7ddd314ca8de3d4c50788ba785339e6f0
GHR <g.rosuson@gmail.com>
2026-02-21 20:02:02 +0100

    feat(backend): add emitter

---
+++ backend/src/aop/emitter/types/index.ts
@@ -0,0 +1,23 @@
+import type { ToolTarget } from 'aop/delegator/tools/types';
+
+/**
+ * A job target event type.
+ */
+type JobTargetEventType = 'job-target-finished';
+
+/**
+ * An event type.
+ */
+type EventType = JobTargetEventType;
+
+/**
+ * A job target event.
+ */
+interface JobTargetEvent extends ToolTarget {
+    jobId: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+type EmitterCallback = (event: JobTargetEvent) => void;
+
+export type { EmitterCallback, JobTargetEvent, EventType };
